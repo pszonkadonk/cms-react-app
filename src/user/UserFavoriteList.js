6d44660e52e4292e73823c131e514b4d2910d8e0
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-08-27 14:28:40 -0400

    create table for faovrite list

--- src/user/UserFavoriteList.js
+++ src/user/UserFavoriteList.js
@@ -25,20 +25,19 @@ class UserFavoriteList extends Component {
             console.log(users);
 
             axios.get(`/retrieve-favorites/${this.props.match.params.structureSlug}`).then((response) => {
+                console.log("response");
+                console.log(response.data);
 
-                console.log(response);
-
+                if(response.data.error) {
+                    alert(response.data.error);
+                    return;
+                }
                 
-
-
-
-            })
+                this.setState({
+                    users: response.data
+                });
+            });
         }); 
-
-        
-
-
-
 
     }
     // fetchFavorites() {
@@ -57,11 +56,34 @@ class UserFavoriteList extends Component {
     render() {
         return(
             <div>
-                <h5>User Favorite List</h5>
-            </div>
+                <h2>Favorites for {this.props.match.params.structureSlug}</h2>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Entry</th> 
+                    </tr>
+                    </thead>
+                    {this.state.users.map(element =>
+                    <tbody>
+                        {element.filteredFavs.map(fav =>
+                            <tr>
+                                <td>{element.username}</td>
+                                <td>{fav.entrySlug}</td>              
+                            </tr>
+                            )}
+                    </tbody>          
+                        )}  
+            </table>
+        </div>
         )
     }
 }
 
 
 export default UserFavoriteList;
+
+// {element.filteredFavs.map(fav =>
+//     <tr key={fav.key}>
+//     </tr>
+//     )}
